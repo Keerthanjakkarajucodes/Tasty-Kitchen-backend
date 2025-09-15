8133763cb473dad2b2aa0866ac637b477c18e62f
Keerthan Jakkaraju <keerthan14.codes@gmail.com>
2025-09-15 17:12:41 +0530

    added .env

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import passport from "passport";
 import { register, login, logout } from "../controllers/authController.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -9,16 +10,38 @@ router.get(
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
-// Google callback route
+router.get("/validate-token", (req, res) => {
+  const token = req.cookies.token;
+  if (!token) return res.json({ valid: false });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return res.json({ valid: true, user: decoded });
+  } catch (err) {
+    return res.json({ valid: false });
+  }
+});
+
 router.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
-    // ✅ Successful login
-    // You can send a JWT token or redirect to frontend
-    res.redirect("https://tasty-kitchen-swiggy-clone.vercel.app/"); // change to your frontend page
+    const token = jwt.sign(
+      { id: req.user._id, email: req.user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.redirect("https://tasty-kitchen-swiggy-clone.vercel.app/");
   }
 );
+
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
